f56ce49c4a36594923d6c97ff3a177366668d668
onava <enava23@yahoo.com>
2021-09-23 00:25:39 -0400

    schema update / prompt created

--- app.js
+++ app.js
@@ -3,4 +3,52 @@ const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 require('dotenv').config();
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const runEmpMan = () => {
+  inquirer
+    .prompt({
+      name: 'main',
+      type: 'list',
+      message: 'What do you want to do?',
+      choices: ['View', 'Add', 'Update', 'Delete', 'End']
+    })
+    .then((chosen) => {
+      switch(chosen.main) {
+        case 'View':
+          viewFunction();
+        break;
+        case 'Add':
+          addFunction();
+        break;
+        case 'Update':
+          updateFunction();
+        break;
+        case 'Delete':
+          deleteFunction();
+        break;
+    
+        default:
+          console.log('Goodbye')
+          connection.end();
+      }
+    })
+}
+
+
+
+
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Welcome to Employee Manager App");
+  runEmpMan();
+})
 
--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS employee_DB;
 CREATE DATABASE employee_DB;
 USE employee_DB;
 
-CREATE TABLE departments (
+CREATE TABLE department (
   id INTEGER AUTO_INCREMENT NOT NULL,
   PRIMARY KEY (id),
   name VARCHAR(30) NOT NULL
@@ -12,7 +12,7 @@ CREATE TABLE role (
   id INTEGER AUTO_INCREMENT NOT NULL,
   PRIMARY KEY (id),
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NUL,
+  salary DECIMAL NOT NULL,
   department_id INTEGER NOT NULL
 );
 
@@ -22,5 +22,12 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
-  manager_id INTEGER,
+  manager_id INTEGER
 );
+
+INSERT INTO department (name) VALUES ("Design"), ("Development"), ("Marketing"), ("Sales");
+INSERT INTO role (title, salary, department_id) VALUES ("Graphic Designer", 40000, 1), ("Senior Graphic Designer", 75000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Web Developer", 45000, 2), ("Senior Web Developer", 90000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Manager", 40000, 3), ("Analyst", 75000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Sales", 40000, 4), ("Senior Sales", 75000, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Omar", "Nava", 1, 1), ("Miguel", "Rodriguez", 2, 2), ("Brandon", "Smith", 3, NULL), ("Jessica", "Jones", 1, NULL);
